0d7d177606e8902113cba26858d325e922d3eed3
clint-gitahi <clingitahi@gmail.com>
2019-09-04 12:25:26 +0300

    using axios to request a different server

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,6 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const _ = require('lodash');
-const users = [
-  { id: '23', firstName: 'clinton', age: 26 },
-  { id: '24', firstName: 'manje', age: 27 }
-];
+const axios = require('axios');
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -23,7 +18,10 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        console.log('parent value', parentValue);
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
